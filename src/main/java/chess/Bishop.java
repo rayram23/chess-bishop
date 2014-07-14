6105ad9042eea76008cd87a9009b7a4d62a6349b
Ray Ramjiawan <rayram23@gmail.com>
2014-07-14 17:26:21 -0400

    Committing from coding interview

--- src/main/java/chess/Bishop.java
+++ src/main/java/chess/Bishop.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -11,6 +12,42 @@ public class Bishop {
      * This method should retrieve the possible positions of a piece on a chess board.
      */
     public Set<Position> getNextPositions(Position origin) {
-        return null;
+       
+    	Set<Position> validPositions = new HashSet<Position>();
+    	//northeast
+    	for(int i = origin.getRow(); i <= Position.MAX_ROW; i++){
+    		for(char c = origin.getColumn(); c <= Position.MAX_COLUMN; c++ ){
+    			Position ne = new Position(c,i);
+    			validPositions.add(ne);
+    		}
+    	}
+    	//northwest
+    	//southeast
+    	//southwest
+    	
+    	return validPositions;
     }
+    private void genreateAllPostion(Set<Position> positions, Position currentPosition){
+    	char currentColumn = currentPosition.getColumn();
+    	int currentRow = currentPosition.getRow();
+    	if(currentRow < Position.MAX_ROW){
+    		if(currentColumn < Position.MAX_COLUMN){
+    			char nextColumn = currentColumn++;
+    			int nextRow = currentRow++;
+    			Position northEast = new Position(nextColumn,nextRow);
+    			positions.add(northEast);
+    			this.genreateAllPostion(positions, northEast);
+    		}
+    		if(currentColumn > Position.MIN_COLUMN){
+    			char nextColumn = currentColumn--;
+    			int nextRow = currentRow++;
+    			Position northWest = new Position(nextColumn,nextRow);
+    			positions.add(northWest);
+    			this.genreateAllPostion(positions, northWest);
+    		}
+    	}
+    	
+    	
+    }
+   
 }
--- src/test/java/chess/BishopTest.java
+++ src/test/java/chess/BishopTest.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,6 +24,15 @@ public class BishopTest {
     @Test
     public void testNextPositionsNotNull() throws Exception {
         assertNotNull("The set of next positions can never be null", bishop.getNextPositions(start));
+    }
+    @Test
+    public void testCantGoFartherNorthEast(){
+    	Position maxNE = new Position(Position.MAX_COLUMN,Position.MAX_ROW);
+    	Bishop b  = new Bishop();
+    	Set<Position> shouldBeEmpty = b.getNextPositions(maxNE);
+    	assertNotNull(shouldBeEmpty);
+    	assertTrue(shouldBeEmpty.isEmpty());
+    	
     }
 
 }
